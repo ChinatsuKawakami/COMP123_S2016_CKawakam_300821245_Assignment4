1e25ae664e61f9e7068d26fa4908e8d4fdd7a2d9
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-07-06 00:29:42 -0400

    initial commit

---
+++ COMP123_S2016_CKawakam300821245_Assignment3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * This is driver class
+ * Author: Chinatsu Kawakami
+ * StudentID:300821245
+ * Date: 6th July 2016
+ * Date Modified : 6th July 2016
+ * Description: This program display the result of two dices sum and roll them 36000 times
+ * version 0.0.1 - initial commit
+ */
+namespace COMP123_S2016_CKawakam300821245_Assignment4
+{
+    /**
+     * The Maim method for our driver class program
+     * @class: program
+     */
+    class Program
+    {
+      /**
+       * The Main method for our driver class program
+       * 
+       * @constructor:Main
+       * @param {string[]}args
+       */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
